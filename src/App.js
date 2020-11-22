cfcdb1b6ea9da24f456ed2c98e1e6fd18598743a
LililiDada <18819774012@163.com>
2020-11-23 00:29:28 +0800

    配置less

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./common.less";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="hhh">哈哈</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
